a9be6a1d9dba33891201ceb73101f5991768a85c
chebon254 <kelvinchebon90@gmail.com>
2023-09-12 01:47:14 +0300

    Setuo

---
+++ Lawyers/js/scripts.js
@@ -0,0 +1,43 @@
+/* == BACK TO TOP == */
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  /* == BACK TO TOP == */
+  
+  
+  /* == ANIMATION CSS == */
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 1;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("showreveal");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  /* == ANIMATION CSS == */
+  
+  //Create A Responsive Menu Hide Show Side Menu
+  const menuBtn = document.querySelector(".menu-btn");
+  const cancelBtn = document.querySelector(".cancel-btn");
+  const menu = document.querySelector(".links");
+  
+  // Onclick For Menu Bar
+  menuBtn.onclick = () => {
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+  }
+  
+  // Onclick For Cancel Menu 
+  cancelBtn.onclick = () => {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+  }
